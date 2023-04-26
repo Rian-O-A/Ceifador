5deaa23500c7ff67ad0c5f121e097ead65363e5d
Rian Oliveira <rianoliveira38@gmail.com>
2023-04-25 22:38:10 -0300

    att

--- mind/command.py
+++ mind/command.py
@@ -31,11 +31,14 @@ async def centralizar(ctx):
             for men in na.members:
                 await men.move_to(canal)
 
+    await ctx.send("Tarefa concluida meu senhor!")
+
 
 @bot.command()
 async def clear(ctx):
     channel = ctx.channel
     messages = []
+    await ctx.send("Sim meu senhor, limparei toda essa imundice!")
     async for message in channel.history(limit=200):
         messages.append(message.id)
     
@@ -43,7 +46,20 @@ async def clear(ctx):
         message = await channel.fetch_message(id)
         await message.delete()
         
-          
+@bot.command()
+async def muteall(ctx):
+    # Verifica se o autor do comando está em um canal de voz
+    await ctx.send("Todos calem a boca, seus mortais!")
+    if ctx.author.voice is None:
+        await ctx.send("Meu senhor, você precisa está em algum canal de voice!")
+        return
+
+    # Obtém o canal de voz em que o autor do comando está
+    voice_channel = ctx.author.voice.channel
+
+    # Percorre todos os usuários no canal de voz e os muta
+    for member in voice_channel.members:
+        await member.edit(mute=True)         
    
 
 
